a97767bfbc8fca4e47bed28213c0df10a4687c35
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-01-23 12:54:34 -0300

    html components

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Task from './src/components/Tasks';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.secondTitle}>Today's tasks</Text>
+        <View style={styles.item}>
+          {/* This is where the tasks will go! */}
+          <Task />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED'
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  secondTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  item: {
+
   },
 });
---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const Task = () => {
+  return (
+    <View>
+      <Text>This is a task</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Task;
